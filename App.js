2751d9e447d5b838ab01fe96f1f24d98687722cd
Aono Ryuju <ryu050101@gmail.com>
2020-05-16 21:23:42 +0900

    Make MemoDetailScreen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import BodyText from './src/elements/BodyText';
 
 import Appbar from './src/components/appbar';
 import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 
 export default function App() {
--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,9 +3,19 @@ import { StyleSheet, View, Text } from 'react-native';
 
 class CircleButton extends React.Component{
     render() {
+        const { style, color } = this.props;
+
+        let bgColor = '#E31676';
+        let textColor = '#fff';
+
+        if(color === 'white'){
+          bgColor = '#fff';
+          textColor = '#E31676';
+        }
+
         return (
-            <View style = {styles.CircleButton}>
-                <Text style = {styles.CircleButtonTitle}>
+            <View style = {[styles.CircleButton, style, {backgroundColor: bgColor}]}>
+                <Text style = {[styles.CircleButtonTitle, {color: textColor}]}>
                   {this.props.children}
                 </Text>
             </View>
@@ -20,14 +30,13 @@ const styles = StyleSheet.create({
       right: 32,
       width: 48,
       height: 48,
-      backgroundColor: '#f72c50',
       borderRadius: 24,
       justifyContent: 'center',
       alignItems: 'center',
-  
+
       shadowColor: '#000',
-      shadowOffset: { width: 0, height: 2},
-      shadowOpacity: 0.9,
+      shadowOffset: { width: 0, height: 5},
+      shadowOpacity: 0.2,
       shadowRadius: 3,
     },
     CircleButtonTitle: {
